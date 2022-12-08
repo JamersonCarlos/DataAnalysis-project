c879bc3ea8d31b3d59b8b05f9e6fcb8bd8708635
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2022-12-07 23:06:53 -0300

    Removing native language functions

--- Aula1-04-12/aula1.py
+++ Aula1-04-12/aula1.py
@@ -1,1 +1,28 @@
-print("HEllo World!")
+#Tipos de dados 
+v = 10 
+nota = 1.2
+nome = "jamerson"
+y = True 
+
+x = int(input("X: "))
+y = int(input("Y: "))
+print(f"Resultado {x} + {y} = {x + y}")
+
+#Operadores relacionais 
+print(nota == 10 and y == True)
+print(nota == 10 or y == True)
+
+#Operadores de comparação 
+print(nota <= 10) 
+print(nota < 10) 
+print(nota > 10) 
+print(nota >= 10)
+print(nota == 10)  
+
+#Tempo Jogo 
+HI = int(input("Hora Inicial: "))
+MI = int(input("Minuto Inicial: "))
+HF = int(input("Hora Final: "))
+MF = int(input("Minuto Final: "))
+
+HTotais = HI - HF
